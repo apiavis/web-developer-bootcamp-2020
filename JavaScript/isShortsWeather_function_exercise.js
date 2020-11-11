899e31b9f689705cc21305a949bff9bad063db5c
Alicia Piavis <aliciapiavis@Alicias-MacBook-Pro.local>
2020-11-11 08:25:26 -0700

    create file for isShortsWeather_function_exercise

---
+++ JavaScript/isShortsWeather_function_exercise.js
@@ -0,0 +1,11 @@
+function isShortsWeather(temp) {
+    if (temp >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+isShortsWeather(80);
+isShortsWeather(25);
+isShortsWeather(75);
